9d322ae4b270ccd3c8d6d2cd811ed259b3f5392c
Prithvi Sathyamoorthy Veeran <zlac206@live.rhul.ac.uk>
2024-10-23 21:35:48 +0100

    More UI changes

--- control.gd
+++ control.gd
@@ -3,9 +3,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/StartButton.connect("pressed", Callable(self, "_on_StartButton_pressed"))
-	$VBoxContainer/OptionsButton.connect("pressed", Callable(self, "_on_OptionsButton_pressed"))
-	$VBoxContainer/ExitButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
+	$StartButton.connect("pressed", Callable(self, "_on_StartButton_pressed"))
+	$OptionsButton.connect("pressed", Callable(self, "_on_OptionsButton_pressed"))
+	$ExitButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
 
 
 func _on_start_button_pressed():
