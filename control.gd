180d9a66456da3be9e54fac579129df7f65663f4
Prithvi Sathyamoorthy Veeran <zlac206@live.rhul.ac.uk>
2025-02-17 21:53:28 +0000

    Control refactored and changed to call the procedrual generated dungeon

--- control.gd
+++ control.gd
@@ -1,24 +1,18 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	$StartButton.connect("pressed", Callable(self, "_on_StartButton_pressed"))
-	$OptionsButton.connect("pressed", Callable(self, "_on_OptionsButton_pressed"))
-	$ExitButton.connect("pressed", Callable(self, "_on_ExitButton_pressed"))
-
+	$StartButton.pressed.connect(_on_start_button_pressed)
+	$OptionsButton.pressed.connect(_on_options_button_pressed)
+	$ExitButton.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_button_pressed():
-	#Loads the game state screen
-	get_tree().change_scene_to_file("res://MainGameEnvironment.tscn")
-
+	get_tree().change_scene_to_file("res://DungeonGenerator.tscn")
 
 func _on_options_button_pressed():
-		#Loads the options menu
+	# Loads the options menu
 	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
-
 
 func _on_exit_button_pressed():
 	# Quit the game
 	get_tree().quit()
-	
