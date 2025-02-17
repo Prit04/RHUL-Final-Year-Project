36a691fa745951cafd93757fcf04463723ac9d33
Prithvi Sathyamoorthy Veeran <zlac206@live.rhul.ac.uk>
2025-02-17 22:26:09 +0000

    Main menu upgraded and redone

--- control.gd
+++ control.gd
@@ -1,18 +1,15 @@
 extends Control
 
-
 func _ready():
-	$StartButton.pressed.connect(_on_start_button_pressed)
-	$OptionsButton.pressed.connect(_on_options_button_pressed)
-	$ExitButton.pressed.connect(_on_exit_button_pressed)
+	$VBoxContainer/StartButton.pressed.connect(_on_start_button_pressed)
+	$VBoxContainer/OptionsButton.pressed.connect(_on_options_button_pressed)
+	$VBoxContainer/ExitButton.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://DungeonGenerator.tscn")
 
 func _on_options_button_pressed():
-	# Loads the options menu
 	get_tree().change_scene_to_file("res://OptionsMenu.tscn")
 
 func _on_exit_button_pressed():
-	# Quit the game
 	get_tree().quit()
