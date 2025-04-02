ab7e18c3bb9b026f6e0251bc2bd7b70b732840af
Prithvi Sathyamoorthy Veeran <zlac206@live.rhul.ac.uk>
2025-04-03 00:17:54 +0100

    Credits commented - found changes to be made in Menus eg: missing labels and positioning of assets

--- credits.gd
+++ credits.gd
@@ -1,14 +1,21 @@
 extends Control
 
+## --- Credits Menu Script ---
+## Handles the back button and plays click sound before returning to main menu.
+
 @onready var back_button = $BackButton
-@onready var click_player = $ClickPlayer  
+@onready var click_player = $ClickPlayer
 
 func _ready():
+	# Connect back button 
 	if back_button:
 		back_button.pressed.connect(_on_back_pressed)
 
-func _on_back_pressed():
+
+func _on_back_pressed() -> void:
+	# Play click sound before returning to main menu
 	if click_player:
 		click_player.play()
 		await click_player.finished
+
 	get_tree().change_scene_to_file("res://control.tscn")
